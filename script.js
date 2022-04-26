8b2e284af3cabf471b2d8f3780bb67272d642ba5
Tonyavall <tonyavallescas@gmail.com>
2022-04-26 10:25:38 -0700

    Fixed military time conversion function from if statement to switch statements.

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ let timeBlock = document.querySelectorAll('.time-block')
 // console.log(timeBlock)
 
 renderInput();
-currentHour();
 
 // Using jquery to set the currentDay text content to grabCurrentDate
 $('#currentDay').text(grabCurrentDate);
@@ -76,37 +75,81 @@ function renderInput() {
     };
 }
 
+let militaryHour = parseInt(grabCurrentHour.slice(0, 2));
+// Check to see if military hour is an integer and is sliced properly
+// console.log(militaryHour)
+
+// Switch Statements to convert normal hours to military time
+switch (grabCurrentHour) {
+    case '12 am':
+        militaryHour = 0;
+        console.log('It is currently ' + militaryHour + ' hours');
+        break;
+    case '1 am':
+    case '2 am':
+    case '3 am':
+    case '4 am':
+    case '5 am':
+    case '6 am':
+    case '7 am':
+    case '8 am':
+    case '9 am':
+    case '10 am':
+    case '11 am':
+    case '12 pm':
+        console.log('It is currently ' + militaryHour + ' hours')
+        break;
+    case '1 pm':
+    case '2 pm':
+    case '3 pm':
+    case '4 pm':
+    case '5 pm':
+    case '6 pm':
+    case '7 pm':
+    case '8 pm':
+    case '9 pm':
+    case '10 pm':
+    case '11 pm':
+    case '12 pm':
+        militaryHour += 12;
+        console.log('It is currently ' + militaryHour + ' hours')
+        break;
+}
+
+  // Function that was replaced by above
+// // Function that converts the current grabbed hour to military time
+// function toMilitaryTime() {
+//     // If the current hour is PM and not 12 
+//     if (grabCurrentHour.slice(-2) === 'pm' && grabCurrentHour.slice(0, 1) !== '12') {
+//         let militaryHour = parseInt(grabCurrentHour.slice(0, 1)) + 12;
+//         return militaryHour
+//     // If the current hour is 12 am
+//     } else if (grabCurrentHour.slice(-2) === 'am' && grabCurrentHour.slice(0, 1) === '12') {
+//         let militaryHour = 0;
+//         return militaryHour
+//     } else if (grabCurrentHour.slice(-2) === 'pm' && grabCurrentHour.slice(0, 1) === '12') {
+//         let militaryHour = 12;
+//         return militaryHour
+//     };
+// }
+
+currentHour();
+
 // function that sets the classes of each timeblock depending on what time it is
 function currentHour() {
-    let currentMilitaryTime = toMilitaryTime()
     // Loops over the time blocks and sets classes past future or present
     for (var i = 0; i < timeBlock.length; i++) {
         // If the current hour timeblock is the present
-        if (parseInt(timeBlock[i].dataset.hour) === currentMilitaryTime) {
+        if (parseInt(timeBlock[i].dataset.hour) === militaryHour) {
             timeBlock[i].setAttribute('id', 'present');
             
         // If the current timeblock is the past
-        } else if (parseInt(timeBlock[i].dataset.hour) < currentMilitaryTime) {
+        } else if (parseInt(timeBlock[i].dataset.hour) < militaryHour) {
             timeBlock[i].setAttribute('id', 'past');
 
         // If the current timeblock is the future
-        } else if (parseInt(timeBlock[i].dataset.hour) > currentMilitaryTime) {
+        } else if (parseInt(timeBlock[i].dataset.hour) > militaryHour) {
             timeBlock[i].setAttribute('id', 'future');
         }
     }
-}
-
-// Function that converts the current grabbed hour to military time
-function toMilitaryTime() {
-    // If the current hour is PM and not 12 
-    if (grabCurrentHour.slice(-2) === 'pm' && grabCurrentHour.slice(0, 1) !== '12') {
-        let militaryHour = parseInt(grabCurrentHour.slice(0, 1)) + 12;
-        return militaryHour
-    // If the current hour is 12 am
-    } else if (grabCurrentHour.slice(-2) === 'am' && grabCurrentHour.slice(0, 1) === '12') {
-        let militaryHour = 0;
-        return militaryHour
-    } else {
-        console.log("The current time is not any of the 2 conditions, fix me")
-    };
 }
